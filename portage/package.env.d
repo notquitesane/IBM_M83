32fade04b759c8a06883844d7ccbd24e62066bad
root <root@HAL.asisee.it>
2021-08-23 03:10:03 -0700

    daily autocommit

---
+++ portage/package.env.d
@@ -0,0 +1,6 @@
+cross-avr/avr-libc cross-avr/avr-libc.conf
+cross-avr/binutils cross-avr/binutils.conf
+cross-avr/gcc cross-avr/gcc.conf
+cross-avr/gdb cross-avr/gdb.conf
+dev-lang/ghc large-tmpdir
+irc/irssi compiler-clang-fno-lto
