85c2d71cdb53cc93a29bd1966d1bca275015047e
root <root@HAL.asisee.it>
2021-09-12 03:10:02 -0700

    daily autocommit

--- portage/package.env.d
+++ portage/package.env.d
@@ -4,3 +4,5 @@ cross-avr/gcc cross-avr/gcc.conf
 cross-avr/gdb cross-avr/gdb.conf
 dev-lang/ghc large-tmpdir
 irc/irssi compiler-clang-fno-lto
+sys-fs/e2fsprogs native.conf
+dev-util/android-tools native.conf
