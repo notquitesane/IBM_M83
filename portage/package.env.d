845736d53bebd1c80357082c6458e0428231a874
root <root@HAL.asisee.it>
2021-09-24 03:10:01 -0700

    daily autocommit

--- portage/package.env.d
+++
@@ -1,9 +0,0 @@
-cross-avr/avr-libc cross-avr/avr-libc.conf
-cross-avr/binutils cross-avr/binutils.conf
-cross-avr/gcc cross-avr/gcc.conf
-cross-avr/gdb cross-avr/gdb.conf
-dev-lang/ghc large-tmpdir
-irc/irssi compiler-clang-fno-lto
-sys-fs/e2fsprogs native.conf
-dev-util/android-tools native.conf
-app-admin/keepass native.conf
